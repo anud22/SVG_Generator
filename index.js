4ba8ae72944a1998507c1ef04a1c103c4347d88f
anuD <adilbagi22@gmail.com>
2023-06-26 17:59:14 -0700

    Added Shapes class

---
+++ index.js
@@ -0,0 +1,8 @@
+const questions = require('./lib/questions.js');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+//inquirer.prompt(questions).then((data) => console.log('hi'));
+
+
+
