31e742f6800a24ee689ed3c6076d45167a3da037
anuD <adilbagi22@gmail.com>
2023-06-27 12:15:18 -0700

    Added code to generate svg file

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const questions = require('./lib/questions.js');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateSvg = require('./lib/generateSvg.js');
 
-//inquirer.prompt(questions).then((data) => console.log('hi'));
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        writeToFile('logo.svg', data);
+    });
+}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateSvg(data), (error) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        console.log("Generated logo.svg");
+    });
+}
+
+init();
 
 
 
